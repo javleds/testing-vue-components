48fde0203328b2708f9f24f1af629810cf0c7ee7
Javier Ledezma <jledezma@medtrainer.com>
2020-03-22 11:34:41 -0600

    Add my first own test

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,62 @@
-import UserList from '@/exercise-1';
-import { mount } from '@vue/test-utils';
+import UserList from '@/exercise-1'
+import { mount } from '@vue/test-utils'
+
+describe('Exercise1Component', () => {
+  let wrapper = null
+
+  beforeEach(() => {
+    wrapper = mount(UserList)
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  it('should display an empty list if there is not users', () => {
+    expect(wrapper.findAll('li').length).toBe(0)
+  })
+
+  it('should display a list users', async () => {
+    const users = ['jledezma']
+
+    wrapper.setProps({ users: [...users] })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('li').length).toBe(1)
+
+    wrapper.setProps({ users: [...users, 'punksolid'] })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('li').length).toBe(2)
+  })
+
+  it('should have a filter input with with binding value', () => {
+    const inputValue = 'jledezma'
+    const input = wrapper.find('input')
+    input.element.value = inputValue
+    input.trigger('input')
+    expect(wrapper.vm.filter).toBe(inputValue)
+  })
+
+  it('should filter uses with reactivity', async () => {
+    const filterValue = 'jledezma'
+    const expectedResults = 1
+
+    const users = ['jledezma', 'punksolid', 'mccelso']
+    wrapper.setProps({ users })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('li').length).toBe(users.length)
+
+    const input = wrapper.find('input')
+    input.element.value = filterValue
+    input.trigger('input')
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('li').length).toBe(expectedResults)
+  })
+
+  it('should display the right user', async () => {
+    const expectedUser = 'jledezma'
+    const users = ['jledezma']
+    wrapper.setProps({ users })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('li').text()).toBe(expectedUser)
+  })
+})
